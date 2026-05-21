aa1939a522b13b0e6305402b4dd814311f8066e2
Lucas Ferreira <lucasferreira059@gmail.com>
2026-05-21 10:18:47 -0300

    schema e model de pagamento criado

--- models/pagamento.py
+++ models/pagamento.py
@@ -6,4 +6,4 @@ class Pagamento (Base):
     __tablename__ = "Pagamento"
     valor = Column(Decimal)
     forma_de_pagamento = Column(String) #aqui vai ser enum
-    status_do_pagamento = Column(String) #aqui vai ser enum
+    status_de_pagamento = Column(String) #aqui vai ser enum
--- schemas/pagamento.py
+++ schemas/pagamento.py
@@ -1,8 +1,21 @@
 from pydantic import BaseModel
 from typing import Optional
 from datetime import date, datetime
+from decimal import Decimal
 from enum import Enum
 
 class Pagamento(BaseModel):
     paciente_id: int
-    
+    valor: Decimal
+    forma_de_pagamento: PagamentoForma
+    status_de_pagamento: PagamentoStatus
+
+class PagamentoForma (str, Enum):
+    pix = "pix"
+    dinheiro = "dinheiro"
+    credito = "credito"
+    debito = "debito"
+
+class PagamentoStatus (bool):
+    pago = False
+    pendente = True
